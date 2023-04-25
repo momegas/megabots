90fa39b159c7333f39ace68502ffdeda577ea310
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2023-04-25 16:50:20 +0530

    feat: deploy using lc-serve

---
+++ megabots/api.py
@@ -0,0 +1,12 @@
+import os
+from megabots import bot
+from lcserve import serving
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+index_dir = os.path.join(cur_dir, "..", "examples", "files")
+
+
+@serving
+def ask(question: str) -> str:
+    mybot = bot("qna-over-docs", index=index_dir)
+    return mybot.ask(question)
